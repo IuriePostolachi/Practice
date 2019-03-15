601d212c01f11dd6472eddee06bc13648616c63c
Iurie Postolachi <47760455+IuriePostolachi@users.noreply.github.com>
2019-03-15 21:33:47 +0200

    практическая работа на Веб-Старт 2.0

---
+++ src/js/forms.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+  $(".modal__form").validate({
+    rules: {
+      username: "required",
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      }
+    },
+    messages: {
+      username: {
+        required: "Пожалуйста введите ваше имя",
+        minlength: jQuery.validator.format("Длина от {0} букв"),
+        maxlength: jQuery.validator.format("Длина до {0} букв")
+      }
+    }
+  });
+  $(".brif-form").validate({
+    rules: {
+      username: "required",
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      username: {
+        required: "Пожалуйста введите ваше имя",
+        minlength: jQuery.validator.format("Длина от {0} букв"),
+        maxlength: jQuery.validator.format("Длина до {0} букв")
+      },
+      email: {
+        required: "На емайл отправим информацию",
+        email: "Введите емайл правильно"
+      }
+    }
+  });
+  $(".offer__form").validate({
+    rules: {
+      username: "required",
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      }
+    },
+    messages: {
+      username: {
+        required: "Пожалуйста введите ваше имя",
+        minlength: jQuery.validator.format("Длина от {0} букв"),
+        maxlength: jQuery.validator.format("Длина до {0} букв")
+      }
+    }
+  });
+});
+$(document).ready(function () {
+  $('.phone').mask('+7 (999) 999-99-99')
+});
